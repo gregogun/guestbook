23cf1d01544783d2c904553f8e6ce128b4503417
gregogun <gregogun97@gmail.com>
2021-08-15 11:16:08 +0100

    updated styling and send/receive functionality

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,12 @@ export default function Home() {
   const [user] = useAuthState(auth)
 
   return (
-    <div className="p-6 mx-auto">
-        <header className="pb-8 flex justify-between w-full justify-center">
-          <h1>Firechat</h1>
+    <div className="mx-auto">
+        <header className="bg-white fixed top-0 p-4 flex justify-between w-full justify-center border-b-2 border-gray-100">
+          <h1 className="text-2xl"><span>🔥</span> Firechat</h1>
           {user && <SignOut />}
         </header>
-        <section className="flex justify-center">
           {user ? <Feed /> : <SignIn />}
-        </section>
     </div>
   )
 }
@@ -48,15 +46,15 @@ function SignIn() {
     auth.signInWithPopup(provider)
   }
   return (
-    <div className="pt-64">
-      <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700" onClick={signInWithGoogle}>Sign in with Google</button>
+    <div className="w-full pt-64 flex">
+      <button className="py-2 px-4 mx-auto display-block font-semibold shadow-md text-white bg-black hover:bg-gray-700 rounded-md" onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   )
 }
 
 function SignOut(){
   return auth.currentUser && (
-    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700" onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="py-2 px-4 font-semibold shadow-md text-white bg-black hover:bg-gray-800 rounded-md" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
@@ -87,16 +85,16 @@ function Feed() {
   }
 
   return (
-    <>
-    <main>
-    {messages && messages.map(msg => <Message key={msg.id} message={msg} />)}
-    <div ref={dummy}></div>
-    </main>
-    <form onSubmit={sendMessage}>
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
-      <button type="submit">Send</button>
-    </form>
-    </>
+    <section className="my-12">
+      <main className="p-4 h-4/5 overflow-scroll">
+        {messages && messages.map(msg => <Message key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+      <form className="bg-gray-500 w-full flex justify-between fixed bottom-0" onSubmit={sendMessage}>
+        <input className="h-16 w-4/5 rounded-none bg-gray-200 px-2 text-lg" placeholder="Share something nice here :)" value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button disabled={!formValue} className={`disabled:opacity-50 disabled:cursor-not-allowed rounded-none h-16 w-1/5 py-2 px-4 font-semibold shadow-md text-white bg-blue-500 rounded-none text-lg`} type="submit">Send <span>🕊</span></button>
+      </form>
+    </section>
   )
 }
 
@@ -106,9 +104,11 @@ function Message(props){
   const messageStatus = uid === auth.currentUser.uid ? 'sent' : 'received'
 
   return (
-    <div className="flex my-4 items-center">
-      <img className="rounded-full h-8 w-8" src={photoURL} alt="" />
-      <h3>{text}</h3>
+    <div className={`flex my-6 items-center ${messageStatus === 'sent' ? 'flex-row-reverse' : 'flex-row'}`}>
+      <img className="rounded-full h-14 w-14 mr-2" src={photoURL} alt="" />
+      <div className={`${messageStatus === 'sent' ? 'p-4 bg-black text-white rounded-l-full rounded-tr-full' : 'p-4 bg-gray-200 rounded-r-full rounded-tl-full'} mr-2`}>
+        <h3 className="text-md">{text}</h3>
+      </div>
     </div>
   )
 }
